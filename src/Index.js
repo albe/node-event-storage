8c904b7336a43be6ff2678477024074f2047c74c
Alexander Berl <a.berl@outlook.com>
2017-04-15 00:03:47 +0200

    Separate index entry into own file

--- src/Index.js
+++ src/Index.js
@@ -1,94 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const mkdirpSync = require('mkdirp').sync;
+const Entry = require('./IndexEntry');
+const EntryInterface = Entry.EntryInterface;
 
 Buffer.poolSize = 64 * 1024;
-
-/**
- * This is the interface that an Entry class needs to implement.
- * @interface
- */
-class EntryInterface {
-    static get size() {}
-    static fromBuffer(buffer, offset = 0) {}
-    toBuffer(buffer, offset) {}
-}
-
-/**
- * Assert that the given class is a valid EntryInterface class.
- *
- * @param {function} EntryClass The constructor for the class
- * @throws {Error} if the given class does not implement the EntryInterface methods or has non-positive size
- */
-function assertValidEntryClass(EntryClass) {
-    if (typeof EntryClass !== 'function'
-        || typeof EntryClass.size === 'undefined'
-        || typeof EntryClass.fromBuffer !== 'function'
-        || typeof EntryClass.prototype.toBuffer !== 'function') {
-        throw new Error('Invalid index entry class. Must implement EntryInterface methods.');
-    }
-    if (EntryClass.size < 1) {
-        throw new Error('Entry class size must be positive.');
-    }
-}
-
-/**
- * Default Entry item contains information about the sequence number, the file position, the document size and the partition number.
- */
-class Entry extends Array {
-
-    /**
-     * @param {number} number The sequence number of the index entry.
-     * @param {number} position The file position where the indexed document is stored.
-     * @param {number} size The size of the stored document (for verification).
-     * @param {number} partition The partition where the indexed document is stored.
-     */
-    constructor(number, position, size = 0, partition = 0) {
-        super(4);
-        this[0] = number;
-        this[1] = position;
-        this[2] = size;
-        this[3] = partition;
-    }
-
-    static get size() {
-        return 4 * 4;
-    }
-
-    static fromBuffer(buffer, offset = 0) {
-        let number     = buffer.readUInt32LE(offset, true);
-        let position   = buffer.readUInt32LE(offset +  4, true);
-        let size       = buffer.readUInt32LE(offset +  8, true);
-        let partition  = buffer.readUInt32LE(offset + 12, true);
-        return new this(number, position, size, partition);
-    }
-
-    toBuffer(buffer, offset) {
-        buffer.writeUInt32LE(this[0], offset, true);
-        buffer.writeUInt32LE(this[1], offset +  4, true);
-        buffer.writeUInt32LE(this[2], offset +  8, true);
-        buffer.writeUInt32LE(this[3], offset + 12, true);
-        return Entry.size;
-    }
-
-    get number() {
-        return this[0];
-    }
-
-    get position() {
-        return this[1];
-    }
-
-    get size() {
-        return this[2];
-    }
-
-    get partition() {
-        return this[3];
-    }
-
-}
-
 
 // node-event-store-index V01
 const HEADER_MAGIC = "nesidx01";
@@ -129,7 +45,7 @@ class Index {
         }
 
         EntryClass = EntryClass || Entry;
-        assertValidEntryClass(EntryClass);
+        Entry.assertValidEntryClass(EntryClass);
 
         let defaults = {
             dataDirectory: '.',
