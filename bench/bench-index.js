9579705cc57b637fc032f935702adbb076a6a9f4
Alexander Berl <a.berl@outlook.com>
2021-02-27 17:55:08 +0100

    Add storage bench

--- bench/bench-index.js
+++ bench/bench-index.js
@@ -1,9 +1,9 @@
 const Benchmark = require('benchmark');
 const benchmarks = require('beautify-benchmark');
-const fs = require('fs');
+const fs = require('fs-extra');
 
 const Suite = new Benchmark.Suite('index');
-Suite.on('cycle', () => fs.existsSync('data/.index') && fs.unlinkSync('data/.index'));
+Suite.on('cycle', () => fs.emptyDirSync('data'));
 Suite.on('cycle', (event) => benchmarks.add(event.target));
 Suite.on('complete', () => benchmarks.log());
 Suite.on('error', (e) => console.log(e.target.error));
@@ -13,8 +13,7 @@ const Latest = require('../index');
 
 const WRITES = 1000;
 
-Suite.add('index [stable]', () => {
-	const index = new Stable.Index('.index', { dataDirectory: 'data' });
+function bench(index) {
 	for (let i = 1; i<=WRITES; i++) {
 		index.add(new Stable.Index.Entry(i,2,4,8));
 	}
@@ -27,22 +26,14 @@ Suite.add('index [stable]', () => {
 	}
 	index.close();
 	if (number !== WRITES) throw new Error('Not all entries were written! Last entry was '+number);
+}
+
+Suite.add('index [stable]', () => {
+	bench(new Stable.Index('.index', { dataDirectory: 'data' }));
 });
 
 Suite.add('index [latest]', () => {
-	const index = new Latest.Index('.index', { dataDirectory: 'data' });
-	for (let i = 1; i<=WRITES; i++) {
-		index.add(new Latest.Index.Entry(i,2,4,8));
-	}
-	index.close();
-
-	let number;
-	index.open();
-	for (let entry of index.all()) {
-		number = entry.number;
-	}
-	index.close();
-	if (number !== WRITES) throw new Error('Not all entries were written! Last entry was '+number);
+	bench(new Latest.Index('.index', { dataDirectory: 'data' }));
 });
 
 Suite.run();
---
+++ bench/bench-storage.js
@@ -0,0 +1,39 @@
+const Benchmark = require('benchmark');
+const benchmarks = require('beautify-benchmark');
+const fs = require('fs-extra');
+
+const Suite = new Benchmark.Suite('partition');
+Suite.on('cycle', () => fs.emptyDirSync('data'));
+Suite.on('cycle', (event) => benchmarks.add(event.target));
+Suite.on('complete', () => benchmarks.log());
+Suite.on('error', (e) => console.log(e.target.error));
+
+const Stable = require('event-storage');
+const Latest = require('../index');
+
+const WRITES = 1000;
+
+function bench(storage) {
+	for (let i = 1; i<=WRITES; i++) {
+		storage.write({ doc: 'this is some test document for measuring performance', value: 123.45, amount: 999, number: i });
+	}
+	storage.close();
+
+	let number;
+	storage.open();
+	for (let doc of storage.readRange(1, -1)) {
+		number = doc.number;
+	}
+	storage.close();
+	if (number !== WRITES) throw new Error('Not all documents were written! Last document was '+number);
+}
+
+Suite.add('storage [stable]', () => {
+	bench(new Stable.Storage('storage', { dataDirectory: 'data' }));
+});
+
+Suite.add('storage [latest]', () => {
+	bench(new Latest.Storage('storage', { dataDirectory: 'data' }));
+});
+
+Suite.run();
