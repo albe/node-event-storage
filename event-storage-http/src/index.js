c1e2d086492f691479a23ab2515919e78dbaeac0
Alexander Berl <a.berl@outlook.com>
2026-05-26 22:51:09 +0200

    Split http layer into own project

--- event-storage-http/src/index.js
+++
@@ -1,3 +0,0 @@
-export { default as EventStoreHttpApi, default, createEventStoreHttpServer } from './EventStoreHttpApi.js';
-export { default as HttpEventStream } from './HttpEventStream.js';
-export { default as AsyncReadablePartition } from './AsyncReadablePartition.js';
