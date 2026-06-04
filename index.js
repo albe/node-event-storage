c36c17d9b8efd30bcae8335a523cd58c23bebb74
Alexander Berl <a.berl@outlook.com>
2026-06-04 19:09:45 +0200

    Export matcher logic

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ export { default as EventStream } from './src/EventStream.js';
 export { default as Storage, StorageLockedError } from './src/Storage.js';
 export { default as Index } from './src/Index.js';
 export { default as Consumer } from './src/Consumer.js';
+export { matches, buildRawBufferMatcher } from './src/utils/metadataUtil.js';
